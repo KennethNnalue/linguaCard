93ed69232c6ed4533efcb093e42b00b5f0366ff1
Kenneth Nnalue <kennethnnalue@gmail.com>
2026-06-04 18:18:53 +0200

    fix app port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -43,8 +43,8 @@ async function bootstrap() {
   });
 
   const port = process.env['PORT'] ?? 3001;
-  await app.listen(port);
-  console.log(`API running at http://localhost:${port}/api/v1`);
+  await app.listen(port, '0.0.0.0');
+  console.log(`API running at http://0.0.0.0:${port}/api/v1`);
 }
 
 bootstrap();
